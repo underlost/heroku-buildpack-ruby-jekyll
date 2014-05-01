d0f2b1b4b57c64eed74e7f25608da93af7a90a9f
Tyler Rilling <underlost@gmail.com>
2014-05-01 15:53:18 -0500

    Update ruby.rb

--- lib/language_pack/ruby.rb
+++ lib/language_pack/ruby.rb
@@ -717,7 +717,7 @@ params = CGI.parse(uri.query || "")
   
   def generate_jekyll_site
     puts "Building jekyll site"
-    pipe("env PATH=$PATH bundle exec jekyll serve 2>&1")
+    pipe("env PATH=$PATH bundle exec jekyll build 2>&1")
     unless $? == 0
       error "Failed to generate site with jekyll."
     end
